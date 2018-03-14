2da4bf7124ddc8e3df6b2381696486760263a229
Guilherme Budnieswski <budnieswski@gmail.com>
2018-03-14 14:02:30 -0300

    Showing server data

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,19 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function(){
+
+    $connection = [];
+    foreach ($_SERVER as $key => $value) {
+        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+            continue;
+        }
+        $connection['dbhost'] = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+        $connection['dbname'] = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+        $connection['dbusername'] = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+        $connection['dbpassword'] = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    }
+
+    echo "<pre>"; print_r($connection); print_r($_SERVER); echo "<pre>";
+
 	return 'Testing page';
 });
